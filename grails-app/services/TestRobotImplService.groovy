d5554cdead20680a7bf7d30a7325db6e6f9223a9
davidtrattnig <david.trattnig@gmail.com>
2009-10-25 14:23:29 +0000

    generalized package structure
    git-svn-id: http://svn.codehaus.org/grails-plugins/grails-wave/trunk@56754 832c1c66-4827-0410-8465-ccb17913cd09

--- grails-app/services/TestRobotImplService.groovy
+++ grails-app/services/TestRobotImplService.groovy
@@ -12,7 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import org.bitfusion.grails.wave.*
+
+import org.grails.plugins.wave.GrailsWaveRobot
 import com.google.wave.api.*
 
 /**
